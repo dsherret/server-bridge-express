c2056cdaeb3456e56f60a2a6af0c4f0ff09ab8bf
David Sherret <dsherret@gmail.com>
2016-05-18 21:15:51 -0400

    1.1.0 - Upgrade to typings 1.0

--- src/typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser/ambient/es6-promise/index.d.ts" />
-/// <reference path="browser/ambient/express/index.d.ts" />
-/// <reference path="browser/ambient/mime/index.d.ts" />
-/// <reference path="browser/ambient/mocha/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/serve-static/index.d.ts" />
---
+++ src/typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/es6-promise/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mime/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/serve-static/index.d.ts" />
